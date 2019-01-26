4e21307e6c2e493c0756ae7567bf28ae9391b1fb
SaumilBapat <sbapat@salesforce.com>
2019-01-25 22:40:37 -0500

    Added Prettier / code indentation

--- public/js/user-profile.js
+++ public/js/user-profile.js
@@ -1,44 +1,35 @@
-
-
-
-var $updateButton = $('.update');
-
+var $updateButton = $(".update");
 
 var API = {
-   
-    updateTask: function (updateData) {
-        return $.ajax({
-            type: "PUT",
-            url: "/api/tasks/" ,
-            data: (updateData)
-        })
-    },
-
-    displayTask: function (){
-        return $.ajax({
-            url: "/api/profile/" + $userID,
-            type: "GET"
-        });
-    }
-}
-
-
-var update = function(){
-    $taskID = $(this).data("id");
-    $taskValue = $(this).val();
-    console.log($taskValue)
-    $taskUpdate = parseInt($('#' + $taskID).val());
-    var updateData = {
-        id: $taskID,
-        completed: $taskUpdate
-    }
-    $('#' + $taskID).val('')
-    API.updateTask(updateData).then(function() {
-        location.reload(); 
-        
+  updateTask: function(updateData) {
+    return $.ajax({
+      type: "PUT",
+      url: "/api/tasks/",
+      data: updateData
     });
+  },
+
+  displayTask: function() {
+    return $.ajax({
+      url: "/api/profile/" + $userID,
+      type: "GET"
+    });
+  }
 };
 
+var update = function() {
+  $taskID = $(this).data("id");
+  $taskValue = $(this).val();
+  console.log($taskValue);
+  $taskUpdate = parseInt($("#" + $taskID).val());
+  var updateData = {
+    id: $taskID,
+    completed: $taskUpdate
+  };
+  $("#" + $taskID).val("");
+  API.updateTask(updateData).then(function() {
+    location.reload();
+  });
+};
 
-
-$updateButton.on("click", update); 
+$updateButton.on("click", update);
--- public/js/user-task.js
+++ public/js/user-task.js
@@ -1,52 +1,48 @@
-
 var $userID = $("#userId").data("id");
 
-var $submit = $('#submit');
-
+var $submit = $("#submit");
 
 var API = {
-    newTask: function (data) {
-        return $.ajax({
-            headers: {
-                "Content-Type": "application/json"
-            },
-            type: "POST",
-            url: "/api/tasks",
-            data: JSON.stringify(data)
-        });
-    },
-    updateTask: function (data) {
-        return $.ajax({
-            type: "PUT",
-            url: "/api/tasks" + $userID,
-            data: JSON.stringify(data)
-        })
-    }
-}
-
-
-var taskFormSubmit = function (event) {
-    event.preventDefault();
-    var $taskName = $("#task-name").val();
-    var $measurement = $("#measurement").val();
-    var $goal = $("#goal").val();
-
-
-    var tasks = {
-        userid: $userID,
-        task: $taskName,
-        unit: $measurement,
-        target: $goal,
-        completed: 0
-    };
-    console.log(JSON.stringify(tasks))
-
-    API.newTask(tasks).then(function () {
-        $("#task-name").val('')
-        $("#measurement").val('')
-        $("#goal").val('')
-        alert('Successfully added, add another?')
+  newTask: function(data) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "/api/tasks",
+      data: JSON.stringify(data)
     });
+  },
+  updateTask: function(data) {
+    return $.ajax({
+      type: "PUT",
+      url: "/api/tasks" + $userID,
+      data: JSON.stringify(data)
+    });
+  }
 };
 
-$submit.on("click", taskFormSubmit); 
+var taskFormSubmit = function(event) {
+  event.preventDefault();
+  var $taskName = $("#task-name").val();
+  var $measurement = $("#measurement").val();
+  var $goal = $("#goal").val();
+
+  var tasks = {
+    userid: $userID,
+    task: $taskName,
+    unit: $measurement,
+    target: $goal,
+    completed: 0
+  };
+  console.log(JSON.stringify(tasks));
+
+  API.newTask(tasks).then(function() {
+    $("#task-name").val("");
+    $("#measurement").val("");
+    $("#goal").val("");
+    alert("Successfully added, add another?");
+  });
+};
+
+$submit.on("click", taskFormSubmit);
